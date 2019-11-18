3152fb49f6435ebea0f5a491e48d8be292063672
Javlonbek9393 <44919149+Javlonbek999@users.noreply.github.com>
2019-11-18 18:10:32 +0500

    Auth signup form fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./Components/Navbars/Navbar";
 import MainNav from "./Components/Navbars/MainNav";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Profile from "./Components/Profile/Profile";
 import Chat from './Components/Dashboard/Chat';
 import MyProjects from './Components/Dashboard/MyProjects'
@@ -16,22 +16,25 @@ import SignUpComplete from "./Components/auth/signup/SignUpComplete";
 
 
 
+
 class App extends Component {
     render() {
         return (
             <div>
                 <BrowserRouter>
-                    <MainNav/>
-                    <Navbar/>
-                    <Route exact path={'/dashboard/search_order'} component={SearchOrder}/>
-                    <Route path={'/dashboard/give_order'} component={GiveOrder}/>
-                    <Route path={'/dashboard/projects'} component={MyProjects}/>
-                    <Route path={'/dashboard/chat'} component={Chat}/>
-                    <Route path={'/profile'} component={Profile}/>
-                    <Route path='/signup1' exact component={ SignUp } />
-                    <Route path='/signup2' component={ SignUpVer } />
-                    <Route path='/signup3' component={ SignUpComplete } />
-                    <Route path='/signin' component={ SignIn } />
+                    <MainNav />
+                    <Navbar />
+                    <Route exact path={'/dashboard/search_order'} component={SearchOrder} />
+                    <Route path={'/dashboard/give_order'} component={GiveOrder} />
+                    <Route path={'/dashboard/projects'} component={MyProjects} />
+                    <Route path={'/dashboard/chat'} component={Chat} />
+                    <Route path={'/profile'} component={Profile} />
+                    <Route path='/signup1' exact component={SignUp} />
+                    <Route path='/signup2' component={SignUpVer} />
+                    <Route path='/signup3' component={SignUpComplete} />
+                    <Route path='/signin' component={SignIn} />
+                    
+
                 </BrowserRouter>
 
             </div>
